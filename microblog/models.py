bd0f97c3e87dfa51371361714f503d5e0ae61b15
mubashirin <gracioso@pm.me>
2021-04-21 13:01:44 +0300

    Первый коммит

---
+++ microblog/models.py
@@ -0,0 +1,21 @@
+from core.db import Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import (
+    ForeignKey,
+    Text,
+    Column,
+    String,
+    Integer,
+    DateTime
+)
+
+
+class Post(Base):
+    __tablename__ = 'microblog_post'
+
+    id = Column(Integer, primary_key=True, index=True, unique=True)
+    title = Column(String)
+    text = Column(String(350))
+    date = Column(DateTime)
+    user = Column(Integer, ForeignKey('user.id'))
+    user_id = relationship('User')
