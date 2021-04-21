02368259ff6b60705b3c022fc009c6eda3813c8a
mubashirin <gracioso@pm.me>
2021-04-21 13:41:16 +0300

    Спрятал пароль

--- core/db.py
+++ core/db.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://halid:Halil4983301..?@localhost/fastapi"
+SQLALCHEMY_DATABASE_URL = "postgresql://halid:password@localhost/fastapi"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
 from enum import Enum
+from microblog.models import Post
+from user.models import User
 
 
 app = FastAPI()
+
+
+@app.post('/post/')
+def post_to_blog():
+    ...
